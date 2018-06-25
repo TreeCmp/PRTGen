a9b6b29f3deb2ce0c5533c0d5b201f85647157cd
goluch <goluch@eti.pg.edu.pl>
2018-06-25 21:49:38 +0200

    removed MFC dependences in XGetopt library

--- XGetopt/XGetopt.h
+++ XGetopt/XGetopt.h
@@ -12,12 +12,11 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-#ifndef XGETOPT_H
-#define XGETOPT_H
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
 
 extern int optind, opterr;
 extern TCHAR *optarg;
 
 int getopt(int argc, TCHAR *argv[], TCHAR *optstring);
-
-#endif //XGETOPT_H

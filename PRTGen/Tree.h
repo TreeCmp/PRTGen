3bde1bdc7e02a7cfa9e1f42c7c1dd734f3330480
Tomasz Goluch <goluch@eti.pg.edu.pl>
2025-09-09 16:07:31 +0200

    moved Edge class to external files

--- PRTGen/Tree.h
+++ PRTGen/Tree.h
@@ -3,30 +3,9 @@
 #include <deque>
 #include "ProgressCounter.h"
 #include "Node.h"
+#include "Edge.h"
 
 using namespace std;
-
-class Edge
-{
-public:
-
-	Node *parent, *child;
-	static Edge* edges;
-	static int index;
-	static int count;
-
-	Edge();
-	Edge(Node *a, Node *b);
-	static Edge* Add(Node* a, Node* b);
-	static void erase(deque<Edge*>& edges, Node *n);
-	static void erase(deque<Edge*>& edges, Edge *e);
-	static int swapParent(deque<Edge*>& edges, Edge *e, Node *new_parent);
-	static int swapParent(deque<Edge*>& edges, Node *parent, Node *child, Node *new_parent);
-	static int swapChild(deque<Edge*>& edges, Edge *e, Node *new_parent);
-	static int swapChild(deque<Edge*>& edges, Node *parent, Node *child, Node *new_child);
-	bool pendant();
-	~Edge();
-};
 
 class Tree
 {
